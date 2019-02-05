3529a79e5667f2d4e2204eee0608c57c19c095d5
whitejz <44676264+whitejz@users.noreply.github.com>
2019-02-05 23:26:08 +0000

    Update Plot1.R

--- ExploratoryDataAnalysis_wk4/Plot1.R
+++ ExploratoryDataAnalysis_wk4/Plot1.R
@@ -12,7 +12,10 @@ NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
 
 totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
 
+png("plot1.png")
+
 barplot(totalNEI[, Emissions]
         , names = totalNEI[, year]
         , xlab = "Years", ylab = "Emissions"
 , main = "Emissions over the Years")
+dev.off()
